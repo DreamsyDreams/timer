d5253023a73b3ff92a4957bc10389a9fb13a2285
Valentyn Savich <vsavich_bus@outlook.com>
2021-02-07 17:26:37 -0500

    Rewrote the code

--- start/app.js
+++ start/app.js
@@ -1,7 +1,7 @@
 //Getting values from sessionStorage
-const hours = sessionStorage.getItem('hrs');
-const minutes = sessionStorage.getItem('mins');
-const seconds = sessionStorage.getItem('secs');
+let hours = parseFloat(sessionStorage.getItem('hrs'));
+let minutes = parseFloat(sessionStorage.getItem('mins'));
+let seconds = parseFloat(sessionStorage.getItem('secs'));
 
 //Accessing elements from index.html
 let hoursText = document.getElementById('hrs');
@@ -14,70 +14,89 @@ const pauseButton = document.getElementById('pause');
 const resetButton = document.getElementById('reset');
 
 //Making elements be equal to the values from sessionStorage
-if (parseFloat(hours) < 10) {
-    hoursText.innerText = '0' + hours;
+if (hours < 10) {
+    hoursText.innerText = '0' + hours.toString();
 } else {
-    hoursText.innerText = hours;
+    hoursText.innerText = hours.toString();
 }
-if (parseFloat(minutes) < 10) {
-    minutesText.innerText = '0' + minutes;
+if (minutes < 10) {
+    minutesText.innerText = '0' + minutes.toString();
 } else {
-    minutesText.innerText = minutes;
+    minutesText.innerText = minutes.toString();
 }
-if (parseFloat(seconds) < 10) {
-    secondsText.innerText = '0' + seconds;
+if (seconds < 10) {
+    secondsText.innerText = '0' + seconds.toString();
 } else {
-    secondsText.innerText = seconds;
+    secondsText.innerText = seconds.toString();
 }
 
 //Variable to hold the interval
 let interval;
 
-let secs = parseFloat(seconds);
-let mins = parseFloat(minutes);
-let hrs = parseFloat(hours);
+if (minutes > 0 && seconds === 0) {
+    minutes--;
+    if (minutes < 10) {
+        minutesText.innerText = '0' + minutes.toString();
+    } else {
+        minutesText.innerText = minutes;
+    }
+    seconds = 59;
+    secondsText.innerText = '59';
+}
+
+if (hours > 0 && minutes === 0 && seconds === 0) {
+    hours--;
+    if (hours < 10) {
+        hoursText.innerText = '0' + hours.toString();
+    } else {
+        hoursText.innerText = hours;
+    }
+    minutes = 59;
+    minutesText.innerText = '59';
+    seconds = 60;
+    secondsText.innerText = '59';
+}
 
 function count() {
-    secs--;
-    if (secs < 10) {
-        secondsText.innerText = '0' + secs.toString();
-    } else {
-        secondsText.innerText = secs;
-    }
-
-    if (secs === 0) {
-        secs = 60;
-        if (hrs === 0 && mins === 0) {
-            return;
-        } else {
-            mins--;
-        }
-        if (mins < 10) {
-            minutesText.innerText = '0' + mins.toString();
-        } else {
-            minutesText.innerText = mins;
-        }
-
-        if (mins === 0) {
-            mins = 60;
-            if (hrs === 0) {;
+    if (seconds === 0) {
+        seconds = 59;
+        if (minutes === 0) {
+            minutes = 59;
+            if (hours === 0) {
                 window.clearInterval(interval);
+                return;
             } else {
-                hrs--;
+                hours--;
             }
-            if (hrs< 10) {
-                hoursText.innerText = '0' + hrs.toString();
+            if (hours < 10) {
+                hoursText.innerText = '0' + hours.toString();
             } else {
-                hoursText.innerText = hrs;
+                hoursText.innerText = hours;
             }
-
+        } else {
+            minutes--;
         }
+        if (minutes < 10) {
+            minutesText.innerText = '0' + minutes.toString();
+        } else {
+            minutesText.innerText = minutes;
+        }
+    } else {
+        seconds--;
     }
-
+    if (seconds < 10) {
+        secondsText.innerText = '0' + seconds.toString();
+    } else {
+        secondsText.innerText = seconds;
+    }
+    if (parseFloat(hoursText.innerText) === 0 && parseFloat(minutesText.innerText) === 0 && parseFloat(secondsText.innerText) === 0) {
+        pauseButton.hidden = true;
+        resetButton.hidden = false;
+    }
 }
 
 startButton.addEventListener('click', button => {
-    interval = window.setInterval(count, 1);
+    interval = window.setInterval(count, 1000);
     pauseButton.hidden = false;
     resetButton.hidden = true;
     startButton.hidden = true;
